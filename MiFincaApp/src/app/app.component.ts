9b2c0bf16f43dc97f481b271be0921b2930e8d69
robert0329 <robert_frias2@ucne.edu.do>
2018-07-08 11:56:15 -0400

    Finca 40%
    Finca 40%

--- MiFincaApp/src/app/app.component.ts
+++ MiFincaApp/src/app/app.component.ts
@@ -5,6 +5,7 @@ import { SplashScreen } from '@ionic-native/splash-screen';
 
 import { HomePage } from '../pages/home/home';
 import { ListPage } from '../pages/list/list';
+import { FincasPage } from '../pages/fincas/fincas';
 
 @Component({
   templateUrl: 'app.html'
@@ -13,6 +14,7 @@ export class MyApp {
   @ViewChild(Nav) nav: Nav;
 
   rootPage: any = HomePage;
+  
 
   pages: Array<{title: string, component: any}>;
 
@@ -22,7 +24,8 @@ export class MyApp {
     // used for an example of ngFor and navigation
     this.pages = [
       { title: 'Home', component: HomePage },
-      { title: 'List', component: ListPage }
+      { title: 'List', component: ListPage },
+      { title: 'Fincas', component: FincasPage }
     ];
 
   }
--- MiFincaApp/src/pages/fincas/fincas.ts
+++ MiFincaApp/src/pages/fincas/fincas.ts
@@ -1,5 +1,8 @@
+import { HomePage } from './../home/home';
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms'
+import { ImagePicker } from '@ionic-native/image-picker';
 
 /**
  * Generated class for the FincasPage page.
@@ -14,10 +17,44 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
   templateUrl: 'fincas.html',
 })
 export class FincasPage {
-
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  form: FormGroup;
+  private imageSrc: string;
+  constructor(private imagePicker: ImagePicker,private fb: FormBuilder,public navCtrl: NavController, public navParams: NavParams) {
+    this.crearFormulario();
+    
+    
   }
-
+  
+  crearFormulario() {
+    this.form = this.fb.group({
+      id: ['',Validators.required],
+      nombre: ['', Validators.required],
+      descripcion: ['', Validators.required],
+      foto: ['', Validators.required]
+    });
+  }
+  private openGallery (): void {
+    let cameraOptions = {
+      sourceType: ImagePicker.PictureSourceType.PHOTOLIBRARY,
+      destinationType: Camera.DestinationType.FILE_URI,      
+      quality: 100,
+      targetWidth: 1000,
+      targetHeight: 1000,
+      encodingType: Camera.EncodingType.JPEG,      
+      correctOrientation: true
+    }
+  
+    Camera.getPicture(cameraOptions)
+      .then(file_uri => this.imageSrc = file_uri, 
+      err => console.log(err));   
+  }
+  guardarFinca() {
+    
+  }
+  OnGoBack(){
+    this.navCtrl.setRoot(HomePage);
+    //this.navCtrl.popToRoot();
+  }
   ionViewDidLoad() {
     console.log('ionViewDidLoad FincasPage');
   }
